2d1d2eedbf1b468c1cdf496f1e08ad9e7d624823
Sagar Tagline <sgs@taglineinfotech.com>
2024-10-15 17:54:45 +0530

    Add endpoint for the service, sub-services and contact us

---
+++ src/api/our-approach/routes/our-approach.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * our-approach router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::our-approach.our-approach');
