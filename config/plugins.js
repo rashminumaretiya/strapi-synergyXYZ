7f9f8f3d15d534fca908e630310992aa9d8db4cb
Sagar Tagline <sgs@taglineinfotech.com>
2024-10-22 16:32:51 +0530

    Add: CDN upload plugin

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,44 @@
-module.exports = () => ({});
+module.exports = () => ({
+    seo: {
+        enabled: true,
+    },
+    upload: {
+        config: {
+            provider: 'aws-s3',
+            providerOptions: {
+                baseUrl: process.env.CDN_URL,
+                s3Options: {
+                    credentials: {
+                        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+                        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+                    },
+                    region: process.env.AWS_REGION,
+                    params: {
+                        Bucket: process.env.AWS_BUCKET_NAME,
+                    },
+                },
+            },
+        },
+    },
+    email: {
+        config: {
+            provider: 'nodemailer',
+            providerOptions: {
+                host: process.env.EMAIL_HOST,
+                port: process.env.EMAIL_PORT,
+                secure: true, // Use TLS
+                auth: {
+                    user: process.env.EMAIL_USERNAME,
+                    pass: process.env.EMAIL_PASSWORD,
+                },
+                tls: {
+                    rejectUnauthorized: false,
+                },
+            },
+            settings: {
+                defaultFrom: `synergyXYZ <${process.env.EMAIL_FROM_ADDRESS}>`,
+                defaultReplyTo: process.env.EMAIL_FROM_ADDRESS,
+            },
+        },
+    },
+});
