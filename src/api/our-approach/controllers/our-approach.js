07b7b401e2edf806e7497d574acaac81992892cb
rashminumaretiya <91937152+rashminumaretiya@users.noreply.github.com>
2024-10-24 17:27:36 +0530

    Revert "Add internationalization support"

--- src/api/our-approach/controllers/our-approach.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * our-approach controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::our-approach.our-approach');
